6a5997ed57e5017959f3a0f6d8f0294ca5f48568
FIRE <133657078+4SVFIRE@users.noreply.github.com>
2024-07-23 22:40:01 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+total_homework = 12
+total_hours = 1.5
+course_name = 'Python'
+time_for_one_task = total_hours / total_homework
+
+print('Курс',course_name,',всего задач:',total_homework,',затрачено часов:',total_hours,',среднее время выполнения',time_for_one_task,'часа.' )
